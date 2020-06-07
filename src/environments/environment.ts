251e52910f04243548122b1add98674e27f32f2b
Leandro Ferreira <lfn.ferreiradonascimento@gmail.com>
2020-06-06 23:48:56 -0300

    Primeiro commit Stackode, inicializando o Angular

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBCOC17YFDOOHXlT6pFP4Tv9_1XDbx9KlU",
+    authDomain: "stackode-6fa29.firebaseapp.com",
+    databaseURL: "https://stackode-6fa29.firebaseio.com",
+    projectId: "stackode-6fa29",
+    storageBucket: "stackode-6fa29.appspot.com",
+    messagingSenderId: "716217302450",
+    appId: "1:716217302450:web:5729b8ee00f945398e78ab"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
